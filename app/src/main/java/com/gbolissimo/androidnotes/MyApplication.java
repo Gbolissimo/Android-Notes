809b0119e68eec94ef2964b57d797cac24f279c2
Gbolissimo <Gbolissimo@gmail.com>
2018-07-01 09:40:07 +0100

    Update MyApplication.java

--- app/src/main/java/com/gbolissimo/androidnotes/MyApplication.java
+++ app/src/main/java/com/gbolissimo/androidnotes/MyApplication.java
@@ -1,24 +1,15 @@
 package com.gbolissimo.androidnotes;
-
 import android.app.Application;
-
 import com.google.firebase.database.FirebaseDatabase;
-
 public class MyApplication extends Application {
-
     private static MyApplication mInstance;
-
     @Override
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-
         mInstance = this;
     }
-
     public static synchronized MyApplication getInstance() {
         return mInstance;
     }
-
-
 }
