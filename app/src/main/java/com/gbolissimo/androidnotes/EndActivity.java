616784d97fb313a75c6d57963e26379dba100cd2
Gbolissimo <Gbolissimo@gmail.com>
2018-07-01 09:37:27 +0100

    Update EndActivity.java

--- app/src/main/java/com/gbolissimo/androidnotes/EndActivity.java
+++ app/src/main/java/com/gbolissimo/androidnotes/EndActivity.java
@@ -1,24 +1,15 @@
 package com.gbolissimo.androidnotes;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import com.google.firebase.database.DatabaseReference;
-
-
-
 public class EndActivity extends AppCompatActivity {
-
-
-
+    
     DatabaseReference myConnectionsStatusRef;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
-
 
         Intent intent=new Intent(EndActivity.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
